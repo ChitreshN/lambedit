cc2bf6c6c9b9bd976d19fb4a25d6ef7b52d36cf3
chitresh narra <chitreshnarra@gmail.com>
2025-10-05 20:23:32 +0530

    feat: use sequences for the buffer datatype
    Previously, the buffer was a list of lines. This commit changes the
    buffer to a list of sequences. This is a more efficient data structure
    for the buffer.

--- src/Utils/ListUtils.hs
+++ src/Utils/ListUtils.hs
@@ -3,10 +3,8 @@ module Utils.ListUtils where
 import Buffer
 import qualified Data.Sequence as Seq
 
-replaceAt :: Int -> a -> [a] -> [a]
-replaceAt _ _ [] = []
-replaceAt 0 x (_ : xs) = x : xs
-replaceAt n x (y : ys) = y : replaceAt (n - 1) x ys
+replaceAt :: Int -> a -> Seq.Seq a -> Seq.Seq a
+replaceAt = Seq.update
 
-infiniteEmpty :: [Buffer]
-infiniteEmpty = Buffer Seq.Empty Seq.Empty 0 : infiniteEmpty
+emptyBuf :: Seq.Seq Buffer
+emptyBuf = Seq.Empty
