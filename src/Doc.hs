ba6430b1e8c757c2e452237d0a69ebed330ad69a
chitresh narra <chitreshnarra@gmail.com>
2025-10-05 20:23:32 +0530

    feat: use sequences for the buffer datatype
    Previously, the buffer was a list of lines. This commit changes the
    buffer to a list of sequences. This is a more efficient data structure
    for the buffer.

--- src/Doc.hs
+++ src/Doc.hs
@@ -1,22 +1,25 @@
-module Doc where
+module Doc (Doc (cursor), initDoc, updateDoc, printDoc) where
 
 import Buffer
+import Data.Foldable
+import qualified Data.Sequence as S
 import Keys
 import Utils.ListUtils
 
 data Doc = Doc
-  { content :: [Buffer]
+  { content :: S.Seq Buffer
   , cursor :: (Int, Int)
   , depth :: Int
   }
+  deriving (Show)
 
 initDoc :: Doc
-initDoc = Doc infiniteEmpty (0, 0) 0
+initDoc = Doc emptyBuf (0, 0) 0
 
 updateDoc :: Doc -> Key -> Doc
 updateDoc d k = case k of
   ArrowUp -> changeCursorBy d (-1) 0 -- limit checking here
-  ArrowDown -> changeCursorBy d 1 0
+  ArrowDown -> changeCursorBy d 1 0 -- and here (or inside changerCursorBy)
   ArrowLeft -> changeCursorBy d 0 (-1)
   ArrowRight -> changeCursorBy d 0 1
   NewLine -> setCursorToStart (changeCursorBy d 1 0)
@@ -27,29 +30,41 @@ changeCursorBy :: Doc -> Int -> Int -> Doc
 changeCursorBy d r c = d{cursor = newCursor}
  where
   (x, y) = cursor d
-  le = lineEnd (content d !! (x + r))
+  le =
+    lineEnd
+      ( case S.lookup (x + r) (content d) of
+          Just b -> b
+          _ -> Buffer{lineEnd = 0, before = S.empty, after = S.empty}
+      )
   newCursor = (x + r, min le (y + c))
 
 setCursorToStart :: Doc -> Doc
 setCursorToStart d = d{cursor = (x, 0)}
-  where
-    (x, _) = cursor d
+ where
+  (x, _) = cursor d
 
 insertChar :: Doc -> Char -> Doc
 insertChar d k =
   d
-    { content = replaceAt x (insertAt y k (content d !! x)) (content d)
+    { content = replaceAt x (insertAt y k (S.index contentPrev x)) contentPrev
     , cursor = newCursor
     , depth = max (depth d) x
     }
  where
   (x, y) = cursor d
   newCursor = (x, y + 1)
+  contentPrev = case S.lookup x (content d) of
+    Just _ -> content d
+    Nothing -> 
+        S.insertAt
+          x
+          (Buffer{lineEnd = 0, before = S.empty, after = S.empty})
+          (content d)
 
 deleteChar :: Doc -> Doc
 deleteChar d =
   d
-    { content = replaceAt x (deleteAt y (content d !! x)) (content d)
+    { content = replaceAt x (deleteAt y (S.index (content d) x)) (content d)
     , cursor = newCursor
     , depth = max (depth d) x
     }
@@ -58,7 +73,7 @@ deleteChar d =
   newCursor = (x, y - 1)
 
 printDoc :: Doc -> IO ()
-printDoc doc = printList (take d c)
-  where
-    d = depth doc + 1
-    c = content doc
+printDoc doc = printList (take d (toList c))
+ where
+  d = depth doc + 1
+  c = content doc
