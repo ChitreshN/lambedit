b5221d12a4e564968879721959641acc026938f1
Chitresh Narra <chitresh@pentarisc.systems>
2025-11-02 21:20:22 +0530

    fmt: format test files
    Merging this should also close #11 in github issues

--- tests/Tests/BufferTests.hs
+++ tests/Tests/BufferTests.hs
@@ -1,9 +1,9 @@
 module Tests.BufferTests (tests) where
 
-import Test.Tasty
-import Test.Tasty.HUnit
 import Buffer
 import qualified Data.Sequence as S
+import Test.Tasty
+import Test.Tasty.HUnit
 
 -- Helper to create an empty buffer
 emptyBuffer :: Buffer
@@ -14,70 +14,82 @@ bufferFromString :: String -> Buffer
 bufferFromString s = Buffer (S.fromList s) S.empty (length s)
 
 tests :: TestTree
-tests = testGroup "Buffer Tests" [
-    insertTests,
-    deleteTests,
-    insertAtTests,
-    deleteAtTests,
-    getStringTests
+tests =
+  testGroup
+    "Buffer Tests"
+    [ insertTests
+    , deleteTests
+    , insertAtTests
+    , deleteAtTests
+    , getStringTests
     ]
 
 insertTests :: TestTree
-insertTests = testGroup "insert" [
-    testCase "insert into empty buffer" $
+insertTests =
+  testGroup
+    "insert"
+    [ testCase "insert into empty buffer" $
         let buf = insert emptyBuffer 'a'
-        in assertEqual "should contain 'a'" "a" (getString buf),
-    testCase "insert into non-empty buffer" $
+         in assertEqual "should contain 'a'" "a" (getString buf)
+    , testCase "insert into non-empty buffer" $
         let buf = insert (bufferFromString "a") 'b'
-        in assertEqual "should contain 'ab'" "ab" (getString buf)
+         in assertEqual "should contain 'ab'" "ab" (getString buf)
     ]
 
 deleteTests :: TestTree
-deleteTests = testGroup "delete" [
-    testCase "delete from empty buffer" $
+deleteTests =
+  testGroup
+    "delete"
+    [ testCase "delete from empty buffer" $
         let buf = delete emptyBuffer
-        in assertEqual "should be empty" "" (getString buf),
-    testCase "delete from single-char buffer" $
+         in assertEqual "should be empty" "" (getString buf)
+    , testCase "delete from single-char buffer" $
         let buf = delete (bufferFromString "a")
-        in assertEqual "should be empty" "" (getString buf),
-    testCase "delete from multi-char buffer" $
+         in assertEqual "should be empty" "" (getString buf)
+    , testCase "delete from multi-char buffer" $
         let buf = delete (bufferFromString "ab")
-        in assertEqual "should contain 'a'" "a" (getString buf)
+         in assertEqual "should contain 'a'" "a" (getString buf)
     ]
 
 insertAtTests :: TestTree
-insertAtTests = testGroup "insertAt" [
-    testCase "insert at beginning" $
+insertAtTests =
+  testGroup
+    "insertAt"
+    [ testCase "insert at beginning" $
         let buf = insertAt 0 'a' (bufferFromString "bc")
-        in assertEqual "should be 'abc'" "abc" (getString buf),
-    testCase "insert in middle" $
+         in assertEqual "should be 'abc'" "abc" (getString buf)
+    , testCase "insert in middle" $
         let buf = insertAt 1 'b' (bufferFromString "ac")
-        in assertEqual "should be 'abc'" "abc" (getString buf),
-    testCase "insert at end" $
+         in assertEqual "should be 'abc'" "abc" (getString buf)
+    , testCase "insert at end" $
         let buf = insertAt 2 'c' (bufferFromString "ab")
-        in assertEqual "should be 'abc'" "abc" (getString buf)
+         in assertEqual "should be 'abc'" "abc" (getString buf)
     ]
 
 deleteAtTests :: TestTree
-deleteAtTests = testGroup "deleteAt" [
-    testCase "delete at beginning (index 0)" $
+deleteAtTests =
+  testGroup
+    "deleteAt"
+    [ testCase "delete at beginning (index 0)" $
         let buf = deleteAt 0 (bufferFromString "abc")
-        in assertEqual "should do nothing" "abc" (getString buf),
-    testCase "delete at index 1" $
+         in assertEqual "should do nothing" "abc" (getString buf)
+    , testCase "delete at index 1" $
         let buf = deleteAt 1 (bufferFromString "abc")
-        in assertEqual "should be 'bc'" "bc" (getString buf),
-    testCase "delete at index 2" $
+         in assertEqual "should be 'bc'" "bc" (getString buf)
+    , testCase "delete at index 2" $
         let buf = deleteAt 2 (bufferFromString "abc")
-        in assertEqual "should be 'ac'" "ac" (getString buf),
-    testCase "delete at end (index 3)" $
+         in assertEqual "should be 'ac'" "ac" (getString buf)
+    , testCase "delete at end (index 3)" $
         let buf = deleteAt 3 (bufferFromString "abc")
-        in assertEqual "should be 'ab'" "ab" (getString buf)
+         in assertEqual "should be 'ab'" "ab" (getString buf)
     ]
 
 getStringTests :: TestTree
-getStringTests = testGroup "getString" [
-    testCase "empty buffer" $
-        assertEqual "should be empty string" "" (getString emptyBuffer),
-    testCase "buffer with content" $
+getStringTests =
+  testGroup
+    "getString"
+    [ testCase "empty buffer" $
+        assertEqual "should be empty string" "" (getString emptyBuffer)
+    , testCase "buffer with content" $
         assertEqual "should be 'abc'" "abc" (getString (bufferFromString "abc"))
     ]
--- tests/unittests.hs
+++ tests/unittests.hs
@@ -1,12 +1,18 @@
 import Test.Tasty
-import qualified Tests.DocTests
 import qualified Tests.BufferTests
-import qualified Tests.Utils.ListUtilsTests
+import qualified Tests.DocTests
 import qualified Tests.Renderer.ViewPortTests
+import qualified Tests.Utils.ListUtilsTests
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [Tests.DocTests.tests, Tests.BufferTests.tests, Tests.Utils.ListUtilsTests.tests, Tests.Renderer.ViewPortTests.tests]
-
+tests =
+  testGroup
+    "Tests"
+    [ Tests.DocTests.tests
+    , Tests.BufferTests.tests
+    , Tests.Utils.ListUtilsTests.tests
+    , Tests.Renderer.ViewPortTests.tests
+    ]
