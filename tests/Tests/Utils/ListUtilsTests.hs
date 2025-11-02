5ca40bf355de10739da7755fdf8d62a63f05d519
Chitresh Narra <chitresh@pentarisc.systems>
2025-11-02 21:20:22 +0530

    fmt: format test files
    Merging this should also close #11 in github issues

--- tests/Tests/Utils/ListUtilsTests.hs
+++ tests/Tests/Utils/ListUtilsTests.hs
@@ -1,62 +1,70 @@
 module Tests.Utils.ListUtilsTests (tests) where
 
+import qualified Data.Sequence as S
 import Test.Tasty
 import Test.Tasty.HUnit
 import Utils.ListUtils
-import qualified Data.Sequence as S
 
 tests :: TestTree
-tests = testGroup "ListUtils Tests" [
-    replaceAtTests,
-    sliceSeqTests,
-    emptyBufTests
+tests =
+  testGroup
+    "ListUtils Tests"
+    [ replaceAtTests
+    , sliceSeqTests
+    , emptyBufTests
     ]
 
 replaceAtTests :: TestTree
-replaceAtTests = testGroup "replaceAt" [
-    testCase "replace at beginning" $
+replaceAtTests =
+  testGroup
+    "replaceAt"
+    [ testCase "replace at beginning" $
         let s :: S.Seq Int
             s = S.fromList [1, 2, 3]
             newS = replaceAt 0 9 s
-        in assertEqual "should be [9,2,3]" (S.fromList [9,2,3]) newS,
-    testCase "replace in middle" $
+         in assertEqual "should be [9,2,3]" (S.fromList [9, 2, 3]) newS
+    , testCase "replace in middle" $
         let s :: S.Seq Int
             s = S.fromList [1, 2, 3]
             newS = replaceAt 1 9 s
-        in assertEqual "should be [1,9,3]" (S.fromList [1,9,3]) newS,
-    testCase "replace at end" $
+         in assertEqual "should be [1,9,3]" (S.fromList [1, 9, 3]) newS
+    , testCase "replace at end" $
         let s :: S.Seq Int
             s = S.fromList [1, 2, 3]
             newS = replaceAt 2 9 s
-        in assertEqual "should be [1,2,9]" (S.fromList [1,2,9]) newS
+         in assertEqual "should be [1,2,9]" (S.fromList [1, 2, 9]) newS
     ]
 
 sliceSeqTests :: TestTree
-sliceSeqTests = testGroup "sliceSeq" [
-    testCase "slice from beginning" $
+sliceSeqTests =
+  testGroup
+    "sliceSeq"
+    [ testCase "slice from beginning" $
         let s :: S.Seq Int
             s = S.fromList [1, 2, 3, 4, 5]
             sliced = sliceSeq s 0 2
-        in assertEqual "should be [1,2,3]" (S.fromList [1,2,3]) sliced,
-    testCase "slice in middle" $
+         in assertEqual "should be [1,2,3]" (S.fromList [1, 2, 3]) sliced
+    , testCase "slice in middle" $
         let s :: S.Seq Int
             s = S.fromList [1, 2, 3, 4, 5]
             sliced = sliceSeq s 1 3
-        in assertEqual "should be [2,3,4]" (S.fromList [2,3,4]) sliced,
-    testCase "slice till end" $
+         in assertEqual "should be [2,3,4]" (S.fromList [2, 3, 4]) sliced
+    , testCase "slice till end" $
         let s :: S.Seq Int
             s = S.fromList [1, 2, 3, 4, 5]
             sliced = sliceSeq s 2 4
-        in assertEqual "should be [3,4,5]" (S.fromList [3,4,5]) sliced,
-    testCase "slice single element" $
+         in assertEqual "should be [3,4,5]" (S.fromList [3, 4, 5]) sliced
+    , testCase "slice single element" $
         let s :: S.Seq Int
             s = S.fromList [1, 2, 3, 4, 5]
             sliced = sliceSeq s 2 2
-        in assertEqual "should be [3]" (S.fromList [3]) sliced
+         in assertEqual "should be [3]" (S.fromList [3]) sliced
     ]
 
 emptyBufTests :: TestTree
-emptyBufTests = testGroup "emptyBuf" [
-    testCase "is empty" $
+emptyBufTests =
+  testGroup
+    "emptyBuf"
+    [ testCase "is empty" $
         assertBool "should be empty" (S.null emptyBuf)
     ]
