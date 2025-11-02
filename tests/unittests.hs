f997f4e1463652ed7d25edc854c91eca5deb9d1b
Chitresh Narra <chitresh@pentarisc.systems>
2025-11-02 20:57:12 +0530

    tests: add doc tests

--- tests/unittests.hs
+++ tests/unittests.hs
@@ -1,7 +1,9 @@
 import Test.Tasty
+import qualified Tests.DocTests
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" []
+tests = testGroup "Tests" [Tests.DocTests.tests]
+
